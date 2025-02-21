da538177a4b17e1bd88220ac4be053cecbd00fc7
Violinie <hancacopakova@gmail.com>
2025-02-21 19:59:01 +0100

    fix: footer styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -325,39 +325,41 @@ export default function Home() {
 		</section>
 
 		<footer className="bg-indigo-950 px-[120px] py-[68px]">
-			<div className="grid grid-cols-5 gap-8 my-8">
-				<div className="text-white col-span-2 space-y-4">
-					<a href="https://www.investujdopole.cz/"><Image src={LogoLight} alt="Logo Investuj do pole" height={42} /></a>
-					<a className="flex text-2xl" href="mailto:info@investujdopole.cz">info@investujdopole.cz</a>
-					<a className="text-2xl" href="tel:+420775972405"><span className="text-emerald-500">+420</span> 775 972 405</a>
+			<div className="max-w-[1220px] mx-auto">
+				<div className="grid grid-cols-5 gap-8 my-8">
+					<div className="text-white col-span-2 space-y-4">
+						<a href="https://www.investujdopole.cz/"><Image src={LogoLight} alt="Logo Investuj do pole" height={42} /></a>
+						<a className="flex text-2xl" href="mailto:info@investujdopole.cz">info@investujdopole.cz</a>
+						<a className="text-2xl" href="tel:+420775972405"><span className="text-[#b4d6a5]">+420</span> 775 972 405</a>
 
+					</div>
+					<ul className="grid gap-6">
+						<li className="text-white text-lg">O společnosti.</li>
+						<li><a href="" className="text-[#afbbd4]">O nás</a></li>
+						<li><a href="" className="text-[#afbbd4]">Naše nabídka</a></li>
+						<li><a href="" className="text-[#afbbd4]">Kontakt</a></li>
+					</ul>
+					<ul className="grid gap-6">
+						<li className="text-white text-lg">Vzdělávání.</li>
+						<li><a href="" className="text-[#afbbd4]">Blog</a></li>
+						<li><a href="" className="text-[#afbbd4]">Webinář</a></li>
+						<li><a href="" className="text-[#afbbd4]">Newsletter</a></li>
+					</ul>
+					<ul className="grid gap-6">
+						<li className="text-white text-lg">Sledujte nás.</li>
+						<li><a href="" className="text-[#afbbd4]">LinkedIn</a></li>
+						<li><a href="" className="text-[#afbbd4]">Facebook</a></li>
+						<li><a href="" className="text-[#afbbd4]">Instagram</a></li>
+					</ul>
 				</div>
-				<ul className="grid gap-6">
-					<li className="text-white text-lg">O společnosti.</li>
-					<li><a href="" className="text-slate-400">O nás</a></li>
-					<li><a href="" className="text-slate-400">Naše nabídka</a></li>
-					<li><a href="" className="text-slate-400">Kontakt</a></li>
-				</ul>
-				<ul className="grid gap-6">
-					<li className="text-white text-lg">Vzdělávání.</li>
-					<li><a href="" className="text-slate-400">Blog</a></li>
-					<li><a href="" className="text-slate-400">Webinář</a></li>
-					<li><a href="" className="text-slate-400">Newsletter</a></li>
-				</ul>
-				<ul className="grid gap-6">
-					<li className="text-white text-lg">Sledujte nás.</li>
-					<li><a href="" className="text-slate-400">LinkedIn</a></li>
-					<li><a href="" className="text-slate-400">Facebook</a></li>
-					<li><a href="" className="text-slate-400">Instagram</a></li>
-				</ul>
-			</div>
 			<div className="flex justify-between border-t border-slate-600 pt-4">
 				<div className="text-white">
 					<a href="https://www.investujdopole.cz/">Investujdopole.cz</a>|
 					<a href="">Zásady ochrany osobních údajů (GDPR)</a>|
 					<a href="">Zásady cookies</a>
 				</div>
-				<div className="text-slate-400">© Investujdopole s.r.o., 2025</div>
+				<div className="text-[#afbbd4]">© Investujdopole s.r.o., 2025</div>
+			</div>
 			</div>
 		</footer>
 	</div>
